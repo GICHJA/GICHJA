c896cb5723aba713c7d1ab1ea94f286944ba9f8f
AdrienP <prieuxadrien@gmail.com>
2015-05-10 12:39:21 +0200

    reporting et image

--- src/VUE/JMenu.java
+++ src/VUE/JMenu.java
@@ -5,32 +5,56 @@
 package VUE;
 
 import java.awt.*;
+import static java.awt.BorderLayout.CENTER;
 import javax.swing.*;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Style;
+import javax.swing.text.StyledDocument;
 
 /**
  *
  * @author lucaromano
  */
-public class JMenu extends JFrame{
-    
+public class JMenu extends JFrame {
+
     private JTextArea ja1;
     private JPanel p;
     //private JTabbedPane tabbedpane;
-    
-    public JMenu(){
-        
-        p = new JPanel (new FlowLayout());
-        /*tabbedpane = new JTabbedPane();
-        this.getContentPane().add("North", tabbedpane);
-        this.tabbedpane.addTab("Menu", p);*/
-        this.ja1 = new JTextArea("Bienvenue. Ce système permet une aide à la décision pour tout le personnel hospitalier. \n" +
-        "Vous avez 3 possibilités. Vous pouvez dans un premier temps faire une recherche en cliquant sur l’onglet Recherche.\n" +
-        "Dans un second temps, vous pouvez mettre à jour la base de donnée en modifiant, ajoutant ou supprimant des données. Pour cela cliquer sur l’onglet Mise à jour.\n" +
-        "Enfin l’onglet génération permet de créer les tables à partir des informations données (requête sql).");
-        p.add(this.ja1);
-        
+
+    public JMenu() throws BadLocationException {
+
+        p = new JPanel(new BorderLayout());
+        /*tabbedpane = new JTabbFlowLayoutedPane();
+         this.getContentPane().add("North", tabbedpane);
+         this.tabbedpane.addTab("Menu", p);*/
+        String s1 = "Bienvenue.\nCe système permet une aide \n"
+                + "à la décision pour tout le personnel hospitalier. \n\n"
+                + "Vous avez 3 possibilités. \n\n"
+                + "Vous pouvez dans un premier temps \n"
+                + "faire une recherche en cliquant sur l’onglet Recherche.\n\n"
+                + "Dans un second temps, "
+                + "\nvous pouvez mettre à jour la base de donnée en modifiant,"
+                + "\n ajoutant ou supprimant des données. "
+                + "\nPour cela cliquer sur l’onglet Mise à jour.\n\n"
+                + "Enfin l’onglet génération permet de créer "
+                + "\nles tables à partir des informations données (requête sql).";
+        this.ja1 = new JTextArea(s1);
+       // Style defaut = ja1.getStyle("default");
+        //StyledDocument sDoc = (StyledDocument) ja1.getDocument();
+        // sDoc.insertString(0, s1, defaut);;
+        JLabel image = new JLabel(new ImageIcon("image/fondhmin.jpg"));
+        ja1.setOpaque(false);
+        ja1.setBackground(new Color(0, 0, 0, 0));
+        ja1.setOpaque(false);
+       // ja1.setContentAreaFilled(false);
+        // ja1.setBorderPainted(false);
+        //   ja1.getViewport().setOpaque(false);
+
+        p.add(image, BorderLayout.CENTER);
+
+       // p.add("South", this.ja1);
+        p.setOpaque(false);
         this.setContentPane(p);
     }
-    
-}
 
+}
